324a33b03a3e6507f7a1a6954d4dd98dc5bb5abd
vlazo1214 <95390029+vlazo1214@users.noreply.github.com>
2023-01-19 17:32:53 -0500

    Add files via upload

---
+++ base.c
@@ -0,0 +1,14 @@
+// base file for stack machine
+// start/Spring 22/SystemsSoftware/hw1-StackMachine (for vin)
+
+#include "header.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_STACK_HEIGHT 2048
+#define MAX_CODE_LENGTH 512
+
+int main(void)
+{
+    printf("hello\n");
+}
